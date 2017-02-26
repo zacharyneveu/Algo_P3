a94f88604b12e2e65c2c775f8944422fd7348376
Zachary Neveu <Zachary Neveu>
2017-02-26 17:31:29 -0500

    Working Somewhat?

--- NevDec - 3A/main.cpp
+++ NevDec - 3A/main.cpp
@@ -14,29 +14,34 @@
 #include"grid.h"
 #include"dictionary.h"
 
+#define MINLENGTH 2 //Minimum length of words
+
 //Standard Name Space
 using namespace std; //standard name space
 vector<string> search(dictionary list, grid matrix);
 void findMatches(dictionary list, grid matrix);
+int mod(int a, int b);
 
 //Main function
 int main()
 {
-	//dictionary d;
-	//d.read("Dictionary.txt");
-	grid g("input15.txt");
+	dictionary d;
+	d.read("testDictionary.txt");
+	cout<<d<<endl;
+
+	grid g("testGrid.txt");
 	cout<<g<<endl;
-	//get grid file, find words, find matches and print them
-	//findMatches();
+
+	vector<string> results;
+	//search dictionary and grid for matches
+	results = search(d,g);
+	for (int i=0; i<results.size(); i++)
+	{
+		cout<<results.at(i)<<endl;
+	}
 	system("pause");
 	return 0;
 }
-
-
-//TODO: What do search and findMatches do? Current understanding: search finds
-//all words in square, findMatches calls search and prints out the words which
-//are matches.  But why is findMatches passed the grid and dictionary then??
-
 
 
 //the search function searches the grid and finds all words in the grid
@@ -48,53 +53,87 @@ vector<string> search(dictionary list, grid matrix)
 	int mody = 0;
 	int rows = matrix.getGrid().rows();
 	int cols = matrix.getGrid().cols();
+	int counter = 0;
 	for (int row = 0; row<rows; row++)
 	{
 		for (int col = 0; col<cols; col++)
 		{
-			for (int length = 5; length<cols; length++)
+			for (int length = MINLENGTH; length<=cols; length++)
 			{
-				for (int k = 0; k<length; k++)
+				for (int direction = 0; direction<8; direction++)
 				{
 					currWord = "";	//Reset currWord each time
-					for (int direction = 0; direction<8; direction++)
+					for (int k = 0; k<length; k++)
 					{
 						switch (direction)
 						{
 						case 0:		//Right
-							modx = (col + k) % cols;
+							modx = mod((col + k), cols);
 							mody = row;
+							break;
 						case 1:		//up right diagonal
-							modx = (col + k) % cols;
-							mody = (row + k) % rows;
+							modx = mod((col + k), cols);
+							mody = mod((row + k), rows);
+							break;
 						case 2:		//up
 							modx = col;
-							mody = (row + k) % rows;
+							mody = mod((row + k), rows);
+							break;
 						case 3:		//up left diagonal
-							modx = (col - k) % cols;
-							mody = (row + k) % rows;
+							modx = mod((col - k), cols);
+							mody = mod((row + k), rows);
+							break;
 						case 4:		//left
-							modx = (col - k) % cols;
+							modx = mod((col - k), cols);
 							mody = row;
+							break;
 						case 5:		//left down diagonal
-							modx = (col - k) % cols;
-							mody = (row - k) % rows;
+							modx = mod((col - k), cols);
+							mody = mod((row - k), rows);
+							break;
 						case 6:		//down
 							modx = col;
-							mody = (row - k) % rows;
+							mody = mod((row - k), rows);
+							break;
 						case 7:		//down right diagonal
-							modx = (col + k) % cols;
-							mody = (row - k) % rows;
+							modx = mod((col + k), cols);
+							mody = mod((row - k), rows);
+							break;
 						}
 						currWord += matrix.getGrid()[mody][modx];
+					}//end k loop
+					if (list.binarySearch(currWord))
+					{
 						allWords.push_back(currWord);
-					}//end direction loop
-				}//end k loop
+						cout<<"Found: "<<currWord<<endl;
+						cout<<"Direction "<<direction<<endl;
+						cout<<"Col "<<col<<endl;
+						cout<<"Row "<<row<<endl;
+						cout<<"Length "<<length<<endl;
+					}
+					else
+					{
+						continue;
+					}
+					//cout<<"k = "<<k<<endl;
+					//cout<<"Direction = "<<direction<<endl;
+				}//end direction loop
+				cout<<"Length = "<<length<<endl;
 			}//end length loop
+			cout<<"Col = "<<col<<endl;
 		}//End columns loop
+		cout<<"Row = "<<row<<endl;
 	}//End rows loop
 	return allWords;
 }//End function
+
+int mod (int a, int b)
+{
+   int ret = a % b;
+   if(ret < 0)
+     ret+=b;
+   return ret;
+}
 
 //findMatches function uses the search function to match words in the dictionary
 //with words found in the grid and prints all found matches
