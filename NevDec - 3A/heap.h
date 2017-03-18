c8afaa5eb0f718ee0372e680c584c3f0a3893839
Zachary Neveu <Zachary Neveu>
2017-03-18 11:09:59 -0400

    Add file descriptions and breakout heap into two files

--- NevDec - 3A/heap.h
+++ NevDec - 3A/heap.h
@@ -1,10 +1,13 @@
-
 #ifndef HEAP_H
 #define HEAP_H
+
 ///Zachary Neveu | neveu.z@husky.neu.edu
 //Jake Dec | dec.j@husky.neu.edu
 //
-//This file includes:
+//This file includes: Definitions for the heap template class which stores data
+//in a vector and includes functions to build and maintain a maxHeap in the
+//vector.  Additionally, this class contains a function to implement heapsort,
+//returning the sorted vector.
 //
 #include<iostream>
 #include<vector>
@@ -62,122 +65,4 @@ public:
     }
 };
 
-//returns parent of object at index passed
-template<class T>
-int heap<T>::parent(const int& index)
-{
-    //parent stored at floor of index/2
-    return (index / 2);
-}
-
-//returns left child of object at index passed
-template<class T>
-int heap<T>::left(const int &index)
-{
-    //left child stored at 2*parent
-    return (index * 2);
-}
-
-//returns right child of object at index passed
-template<class T>
-int heap<T>::right(const int &index)
-{
-    //right child stored at 2*parent+1
-    return (index * 2 + 1);
-}
-
-
-//returns item at index passed
-template<class T>
-T heap<T>::getItem(const int &index)
-{
-    return data[index];
-}
-
-//copies given vector into the heap
-template<class T>
-void heap<T>::initializeMaxHeap(const vector<T> &dictionary)
-{
-
-    data = dictionary;;
-    data.push_back(data[0]);
-    data[0] = " ";
-
-    //initialize heapsize
-    this->heapsize = data.size() - 1;
-}
-
-
-//arranges array to meet maxHeap property
-template<class T>
-void heap<T>::buildMaxHeap()
-{
-
-    for (int i = heapsize / 2; i > 0; i--)
-    {
-        maxHeapify(i);
-    }
-}
-
-
-//maxHeapify ensures that the maxHeap property is upheld at the node
-//specified by the index passed
-template<class T>
-void heap<T>::maxHeapify(const int &index)
-{
-    int l = left(index);
-    int r = right(index);
-    int largest = 0;
-    string temp;
-
-    if (l <= heapsize && data[index] < data[l])
-    {
-        largest = l;
-    }
-    else
-    {
-        largest = index;
-    }
-
-    if (r <= heapsize && data[largest] < data[r])
-    {
-        largest = r;
-    }
-
-    //cout<<"largest: "<<data[largest]<<endl;
-    if (largest != index)
-    {
-        //swap data[i] and data[largest];
-        //cout<<"swapping: "<<data[index]<<"<->"<<data[largest]<<endl;
-        temp = data[index];
-        data[index] = data[largest];
-        data[largest] = temp;
-        this->maxHeapify(largest);
-    }
-}
-
-template<class T>
-vector<T> heap<T>::maxHeapSort()
-{
-    string temp;
-
-    for (int i = heapsize; i > 1; i--)
-    {
-
-        //swap data[i] and data [1] (first and last elements)
-        temp = data[i];
-        data[i] = data[1];
-        data[1] = temp;
-
-        //decrement heap size to exclude sorted value
-        heapsize--;
-
-        //reorganize heap with new top value
-        maxHeapify(1);
-    }
-
-    return data;
-}
-
 #endif
-
